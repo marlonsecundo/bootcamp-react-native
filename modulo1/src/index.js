c7b840122ae30c9f4c18755b723d43fc2256f83d
Marlon Secundo <marlon_secundo@outlook.com>
2019-02-14 19:15:10 -0300

    State and immutability

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
 
 import Todo from "./components/Todo";
 
 export default class App extends Component {
+  state = {
+    usuario: "Marlon",
+    todos: [
+      { id: 1, text: "Fazer café" },
+      { id: 2, text: "Estudar o GoNative" }
+    ]
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "Novo todo" }]
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="Fazer café" />
+        <Text>{this.state.usuario}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="Adicionar todo" onPress={this.addTodo} />
       </View>
     );
   }
