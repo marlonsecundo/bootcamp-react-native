4d2944ba6e143e1002b64e4a2afdd90e6089738b
Marlon Secundo <marlon_secundo@outlook.com>
2019-02-18 14:49:53 -0300

    Fix

---
+++ module1/src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+// import styles from './styles';
+
+const Todo = ({ title }) => (
+  <View>
+    <Text>
+      {' '}
+      {title}
+    </Text>
+  </View>
+);
+
+Todo.defaultProps = {
+  title: 'Todo padrão',
+};
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Todo;
---
+++ module1/src/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, Text, View, Button,
+} from 'react-native';
+import Todo from '~/components/Todo';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+console.tron.log('Hello World');
+
+export default class App extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        text: 'Hello World',
+      });
+    }, 3000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter < 5;
+  }
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  componentWillUnmount() {}
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { text: nextProps.text };
+  }
+
+  handleAddCount = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  state = {
+    counter: 0,
+    usuario: 'Marlon',
+    todos: [{ id: 1, text: 'Fazer café' }, { id: 2, text: 'Estudar o GoNative' }],
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: 'Novo todo' }],
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={this.handleAddCount} title="AddCount" />
+        <Text>{this.state.counter}</Text>
+        <Text>{this.state.usuario}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="Adicionar todo" onPress={this.addTodo} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
