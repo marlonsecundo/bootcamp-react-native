28f03c8ceb9ad2342035016db42ad423c68dea3e
Marlon Secundo <marlon_secundo@outlook.com>
2019-02-18 14:47:47 -0300

    Styles

---
+++ module2/src/pages/Welcome/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const Welcome = () => <View style={styles.container} />;
+
+export default Welcome;
