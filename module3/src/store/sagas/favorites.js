f6544072f708e7f085bf0869274327a4d0444a00
Marlon Secundo <marlon_secundo@outlook.com>
2019-02-24 14:34:22 -0300

    Duplicate Repository

--- module3/src/store/sagas/favorites.js
+++ module3/src/store/sagas/favorites.js
@@ -1,12 +1,19 @@
 import api from '~/services/api';
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import { addFavoriteSuccess, addFavoriteError } from '~/store/actions/favorites';
 
 export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repoName}`);
-    yield put(addFavoriteSuccess(response.data));
+
+    const favorites = yield select(state => state.favorites.data);
+
+    if (favorites.find(favorite => favorite.id === response.data.id)) {
+      yield put(addFavoriteError('Repositorio Duplicado'));
+    } else {
+      yield put(addFavoriteSuccess(response.data));
+    }
   } catch (error) {
     yield put(addFavoriteError('Respoitorio não encontrado'));
   }
